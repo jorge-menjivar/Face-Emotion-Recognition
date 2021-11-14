383a5962421b1ae5e8ef785453f237f60689eb0c
michael-yang2 <michaelyang2k8@gmail.com>
2021-11-14 12:11:10 -0800

    face detection works

--- Face_Detection/face_detect.py
+++ Face_Detection/face_detect.py
@@ -6,7 +6,6 @@ eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml
 #@param: an OpenCV image object
 #@return: an OpenCV image containing only the face
 def detect_face(img):
-	
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 	for (x,y,w,h) in faces:
