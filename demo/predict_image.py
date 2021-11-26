7c34507d5b9cd0c9da2f8ac39d0b5d4d4d99a4da
CrownClown99 <67250507+CrownClown99@users.noreply.github.com>
2021-11-25 17:13:11 -0800

    Update predict_image.py

--- demo/predict_image.py
+++ demo/predict_image.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
+from keras.models import model_from_json
 import numpy as np
 
 
@@ -8,9 +9,15 @@ def run(image: Image):
     input_image = img_to_array(image)
     input_image = input_image.reshape((1, 48, 48, 1))/255
 
-    model = load_model("CNN.h5")
+    json_file = open('CNN2.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("CNN2.h5")
+    print("Loaded model from disk")
 
-    prediction = np.argmax(model.predict(input_image), axis=1)
+    prediction = np.argmax(loaded_model.predict(input_image), axis=1)
 
     emotion_label_to_text = {
         0: 'anger',
